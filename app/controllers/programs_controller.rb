8dc023ed5623382cd99f5099e909fc7fa03c54fd
aalesso <aalesso@users.noreply.github.com>
2017-04-09 01:12:12 -0500

    Sum_of_1000_multiples_challenge

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -10,17 +10,17 @@ class ProgramsController < ApplicationController
     b=possible_plays.sample
     if (a==b)
       result="tie"
-      elsif (a==r && b==s)
+    elsif (a==r && b==s)
       result="a"
-      elsif (a==r && b==p)
+    elsif (a==r && b==p)
       result="b"
-      elsif (a==p && b==s)
+    elsif (a==p && b==s)
       result="b"
-      elsif (a==p && b==r)
+    elsif (a==p && b==r)
       result="a"
-      elsif (a==s && b==r)
+    elsif (a==s && b==r)
       result="b"
-      else
+    else
       result="a"
     end
     @your_output = "A got #{a}, B got #{b}, and the winner is #{result.capitalize}"
@@ -44,19 +44,33 @@ class ProgramsController < ApplicationController
   end
 
   def second_program
-    # Your code goes below.
 
-    @your_output = "Replace this string with your output"
+    our_numbers = [4, 10, 6]        # Create an array of numbers
+    squared_numbers = []            # Create an empty array
+
+    our_numbers.each do |num|       # For each element in numbers, (refer to it as "num")
+      square = num * num            # Square the number
+      squared_numbers.push(square)  # Push it into the squared_numbers array
+    end
+
+    @your_output = squared_numbers.sum  # Sum the squares
 
     render("programs/second_program.html.erb")
   end
 
   def third_program
+    divisors=[]
     numbers = (1..999).to_a
-
+    numbers.each do |num|
+      if (num % 3 ==0) || (num % 5 == 0)
+        divisors.push(num)
+      else
+        1+1 # i dont know what else to put here, i want it to do nothing
+      end
+    end
     # Your code goes below.
 
-    @your_output = "Replace this string with your output"
+    @your_output = divisors.sum
 
     render("programs/third_program.html.erb")
   end
