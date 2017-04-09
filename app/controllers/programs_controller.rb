bf26263fcb195846c81d4de94e970e123a8ed201
aalesso <aalesso@users.noreply.github.com>
2017-04-09 00:30:19 -0500

    challenge_completed

--- app/controllers/programs_controller.rb
+++ app/controllers/programs_controller.rb
@@ -1,16 +1,44 @@
 class ProgramsController < ApplicationController
   def home
     # Your code goes below.
+    r="rock"
+    s="scissors"
+    p="paper"
+    possible_plays=[r,s,p]
 
-    @your_output = "Replace this string with your output"
+    a=possible_plays.sample
+    b=possible_plays.sample
+    if (a==b)
+      result="tie"
+      elsif (a==r && b==s)
+      result="a"
+      elsif (a==r && b==p)
+      result="b"
+      elsif (a==p && b==s)
+      result="b"
+      elsif (a==p && b==r)
+      result="a"
+      elsif (a==s && b==r)
+      result="b"
+      else
+      result="a"
+    end
+    @your_output = "A got #{a}, B got #{b}, and the winner is #{result.capitalize}"
 
     render("programs/home.html.erb")
   end
 
+
+
+
+
+
+
   def first_program
     # Your code goes below.
-
-    @your_output = "Replace this string with your output"
+    if 2 < 1
+      @your_output = "duh"
+    end
 
     render("programs/first_program.html.erb")
   end
